528605152854e73052a4f6c7d93c0ce54415f82c
Sam Ireland <sam.ireland.uk@gmail.com>
2016-04-16 23:59:50 +0100

    Static files setup and footer images

--- samireland/settings.py
+++ samireland/settings.py
@@ -101,5 +101,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "samireland", "static")
+]
 
 STATIC_URL = '/static/'
