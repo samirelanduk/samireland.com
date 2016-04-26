8ddf6f34158a1af67b682d4a79a5bbe9d88db416
Sam Ireland <sam.ireland.uk@gmail.com>
2016-04-26 15:30:29 +0100

    Basic body responsiveness

--- functional_tests/test_style.py
+++ functional_tests/test_style.py
@@ -11,3 +11,18 @@ class CssTest(SamTest):
          body.value_of_css_property("font-family"),
          "'Roboto Slab'"
         )
+
+
+    def test_resize_above_1000px(self):
+        self.browser.get(self.live_server_url + "/")
+        body = self.browser.find_element_by_tag_name("body")
+        self.browser.set_window_size(1100, 800)
+        self.assertEqual(
+         body.value_of_css_property("width"),
+         "1000px"
+        )
+        self.browser.set_window_size(900, 800)
+        self.assertEqual(
+         body.value_of_css_property("width"),
+         "900px"
+        )
