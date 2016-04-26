22e564a714d9a1e9b5ac343d52b91182b485959d
Sam Ireland <sam.ireland.uk@gmail.com>
2016-04-27 00:56:59 +0100

    Update to settings to make compatible with fabric

--- samireland/settings.py
+++ samireland/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'samireland.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    "default": {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
