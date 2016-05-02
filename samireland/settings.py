81060730c0fd14f1f40e81b17666f51c18e69d77
Sam Ireland <sam.ireland.uk@gmail.com>
2016-05-03 00:49:41 +0100

    Even more fabfile changes

--- samireland/settings.py
+++ samireland/settings.py
@@ -81,10 +81,7 @@ WSGI_APPLICATION = 'samireland.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": os.path.join(BASE_DIR, "db.sqlite3")}
 }
 
 
