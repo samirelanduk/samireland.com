6a911b0001bb220ae7d228edf211c5b86a4bb8d9
Sam Ireland <sam.ireland.uk@gmail.com>
2016-05-30 00:47:16 +0100

    Add most recent video to piano page

--- piano/views.py
+++ piano/views.py
@@ -1,8 +1,18 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
 def piano_page(request):
-    return render(request, "piano.html")
+    yt_html = requests.get(
+     "https://www.youtube.com/channel/UCILeIbhtlv4lmgAaZbPKr8A"
+    ).text
+    title_start = yt_html.find('<h3 class="yt-lockup-title ">')
+    href_start = yt_html[title_start:].find("href=")
+    href_end = yt_html[title_start + href_start:].find(">")
+    yt_code = yt_html[
+     title_start + href_start:title_start + href_start + href_end
+    ].split("=")[-1].split('"')[0]
+    return render(request, "piano.html", {"recent_code": yt_code})
 
 
 def practice_page(request):
