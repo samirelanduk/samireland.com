36f4742a9ed8ea74058be1e218f076a24f12263e
Sam Ireland <sam.ireland.uk@gmail.com>
2016-05-29 18:40:19 +0100

    FT for music chords practice

--- functional_tests/test_music.py
+++ functional_tests/test_music.py
@@ -123,3 +123,47 @@ class PracticeAppTest(FunctionalTest):
             next_note = display.text
             self.assertNotEqual(note, next_note)
             note = next_note
+
+
+    def test_can_practice_chords(self):
+        # The user goes to the practice page
+        self.browser.get(self.live_server_url + "/music/practice/")
+
+        # There is a an option to specify the kind of practice to do
+        options = self.browser.find_element_by_id(
+         "options").find_elements_by_class_name("option")
+
+        # The second option is chords - they click it
+        self.assertEqual(
+         options[1].find_elements_by_tag_name("label").text,
+         "Chords"
+        )
+        options[0].find_elements_by_tag_name("input").click()
+
+        # There is also an option to specify the number of seconds
+        seconds = self.browser.find_element_by_id(
+         "seconds").find_elements_by_tag_name("input")
+        seconds.send_keys("1")
+
+        # They start the practice
+        start = self.browser.find_element_by_tag_name("button")
+        display = self.browser.find_element_by_id("display")
+        self.assertEqual(display.text, "")
+        start.click()
+
+        # They go for ten seconds
+        chord = display.text
+        allowed_values = [
+         "A Major", "B Major", "C Major", "D Major", "E Major", "F Major", "G Major",
+        ]
+        for i in range(10):
+            self.assertIn(chord, allowed_values)
+            time.sleep(1)
+            next_chord = display.text
+            self.assertNotEqual(chord, next_chord)
+            chord = next_chord
+
+        # They stop
+        stop = self.browser.find_elemenst_by_tag_name[1]("button")
+        stop.click()
+        self.assertEqual(display.text, "")
