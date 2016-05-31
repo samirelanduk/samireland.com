414d0e4cea346c3e485893931e8e7e7b41ba228a
samirelanduk <sam.ireland.uk@gmail.com>
2016-06-01 00:42:29 +0100

    Piano JS refactor, ready for sheet code

--- piano/static/js/piano.js
+++ piano/static/js/piano.js
@@ -1,5 +1,33 @@
 var runner;
+var canvas;
+var context;
 
+// Resize canvas when page loads
+$(window).load(function() {
+  canvas = document.getElementsByTagName("canvas")[0];
+  canvas.width  = $("main").width() * 0.9;
+})
+
+
+// Prep the canvas when option changed
+$("input[name=options]").on("click", function() {
+  if ($("#stop").is(":disabled")) {
+    if ($("#id_sheets").is(':checked')) {
+      var renderer = new Vex.Flow.Renderer(canvas, Vex.Flow.Renderer.Backends.CANVAS);
+      context = renderer.getContext();
+      var topStaff = new Vex.Flow.Stave(10, -50, canvas.width - 20, {spacing_between_lines_px: 15});
+      var bottomStaff = new Vex.Flow.Stave(10, 50, canvas.width - 20, {spacing_between_lines_px: 15});
+      topStaff.addClef('treble').setContext(context).draw();
+      bottomStaff.addClef('bass').setContext(context).draw();
+    } else {
+      context = canvas.getContext("2d");
+      context.clearRect(0, 0, canvas.width, canvas.height);
+    }
+  }
+})
+
+
+// Start the practice
 $("#start").on("click", function() {
   $('#start').attr("disabled", true);
   $('#stop').attr("disabled", false);
@@ -28,57 +56,31 @@ $("#start").on("click", function() {
   }
 
   if ($("#id_sheets").is(':checked')) {
-    /*var position = notes[Math.floor(Math.random() * notes.length)];
-    $("#display").find("svg").append(
-     "<ellipse id='note' cx='180' cy='" +
-     ((position * 20) + 10) +
-    "' data='" +
-     position +
-     "' rx='10' ry='8' stroke='black' stroke-width='3' fill='black' />"
-    )
-    $("#display").html($("#display").html());
-    runner = window.setInterval(function(){
-      next_notes = notes.slice(0);
-      next_notes.splice(next_notes.indexOf($("#note").attr("data")), 1);
-      position = next_notes[Math.floor(Math.random() * next_notes.length)];
-      $("#note").attr("y", (position * 20) + 10);
-      $("#note").attr("data", (position - 10) / 20);
-    }, seconds * 1000);*/
+    
   } else {
-    var canvas = document.getElementsByTagName("canvas")[0];
-    var context = canvas.getContext("2d");
-    var first_note = notes[Math.floor(Math.random() * notes.length)];
+    context = canvas.getContext("2d");
+    var note = notes[Math.floor(Math.random() * notes.length)];
     context.font = "60px Arial";
     context.textAlign = "center";
     context.textBaseline = "middle";
-    context.fillText(first_note, canvas.width / 2, canvas.height / 2);
+    context.fillText(note, canvas.width / 2, canvas.height / 2);
     runner = window.setInterval(function(){
       next_notes = notes.slice(0);
-      next_notes.splice(next_notes.indexOf($("#display").html()), 1);
+      next_notes.splice(next_notes.indexOf(note), 1);
       context.clearRect(0, 0, canvas.width, canvas.height);
-      context.fillText(next_notes[Math.floor(Math.random() * next_notes.length)], canvas.width / 2, canvas.height / 2);
+      note = next_notes[Math.floor(Math.random() * next_notes.length)]
+      context.fillText(note, canvas.width / 2, canvas.height / 2);
     }, seconds * 1000);
   }
 })
 
 
+// Stop the practice
 $("#stop").on("click", function() {
   $('#start').attr("disabled", false);
   $('#stop').attr("disabled", true);
 
   clearInterval(runner);
-  var canvas = document.getElementsByTagName("canvas")[0];
-  var context = canvas.getContext("2d");
+  context = canvas.getContext("2d");
   context.clearRect(0, 0, canvas.width, canvas.height);
-})
-
-
-$("input[name=options]").on("click", function() {
-  if ($("#stop").is(":disabled")) {
-    if ($("#id_sheets").is(':checked')) {
-      $("#display").html($("#svgcontainer").html());
-    } else {
-      $("#display").html("");
-    }
-  }
 })
