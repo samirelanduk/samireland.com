c51624dd8ec633cbab53fbfa1f0922f6f19573e8
samirelanduk <sam.ireland.uk@gmail.com>
2016-06-01 02:34:32 +0100

    Can now display sheet notes

--- piano/static/js/piano.js
+++ piano/static/js/piano.js
@@ -9,16 +9,54 @@ $(window).load(function() {
 })
 
 
+// For a given tone, decide which staff to use
+function pickStaff(note) {
+  var cleff;
+  if ((note[1] > 4) || ((note[1] === 4) && (["G", "A", "B"].indexOf(note[0]) != -1))) {
+    cleff = "treble";
+  } else if ((note[1] < 3) || ((note[1] === 3) && (["C", "D", "E", "F"].indexOf(note[0]) != -1))) {
+    cleff = "bass";
+  } else {
+    cleff = ["treble", "bass"][Math.floor(Math.random() * 2)];
+  }
+  return cleff;
+}
+
+
+// Paint a staff with an optional note
+function paintGrandStaff(note) {
+  var renderer = new Vex.Flow.Renderer(canvas, Vex.Flow.Renderer.Backends.CANVAS);
+  context = renderer.getContext();
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  var topStaff = new Vex.Flow.Stave(10, 0, canvas.width - 20, {spacing_between_lines_px: 10});
+  var bottomStaff = new Vex.Flow.Stave(10, 100, canvas.width - 20, {spacing_between_lines_px: 10});
+  topStaff.addClef('treble').setContext(context).draw();
+  bottomStaff.addClef('bass').setContext(context).draw();
+  if (note != null) {
+    var allocatedCleff = pickStaff(note);
+    var voice = new Vex.Flow.Voice({
+      num_beats: 1,
+      beat_value: 4,
+      resolution: Vex.Flow.RESOLUTION
+    });
+    var staveNotes;
+    var notes = [
+      new Vex.Flow.StaveNote({ clef: allocatedCleff, keys: [note[0] + "/" + note[1]], duration: "q" })
+      //new Vex.Flow.StaveNote({ clef: "bass", keys: ["D/3"], duration: "q" })
+    ];
+    voice.addTickables(notes);
+    var formatter = new Vex.Flow.Formatter().joinVoices([voice]).format([voice], 500);
+    voice.draw(context,allocatedCleff === "treble" ? topStaff : bottomStaff);
+    //voice.draw(context, bottomStaff);
+  }
+}
+
+
 // Prep the canvas when option changed
 $("input[name=options]").on("click", function() {
   if ($("#stop").is(":disabled")) {
     if ($("#id_sheets").is(':checked')) {
-      var renderer = new Vex.Flow.Renderer(canvas, Vex.Flow.Renderer.Backends.CANVAS);
-      context = renderer.getContext();
-      var topStaff = new Vex.Flow.Stave(10, -50, canvas.width - 20, {spacing_between_lines_px: 15});
-      var bottomStaff = new Vex.Flow.Stave(10, 50, canvas.width - 20, {spacing_between_lines_px: 15});
-      topStaff.addClef('treble').setContext(context).draw();
-      bottomStaff.addClef('bass').setContext(context).draw();
+      paintGrandStaff(null);
     } else {
       context = canvas.getContext("2d");
       context.clearRect(0, 0, canvas.width, canvas.height);
@@ -45,9 +83,14 @@ $("#start").on("click", function() {
      "A Major", "B Major", "C Major", "D Major", "E Major", "F Major", "G Major"
     ]
   } else if ($("#id_sheets").is(':checked')) {
-    notes = [
-     0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5
-    ]
+    var tones = ["C", "D", "E", "F", "G", "A", "B"];
+    var keys = [2, 3, 4, 5];
+    notes = [];
+    for (var t = 0; t < tones.length; t++) {
+      for (var k = 0; k < keys.length; k++) {
+        notes.push([tones[t], keys[k]])
+      }
+    }
   } else {
     notes = [
      "A", "B", "C", "D", "E", "F", "G",
@@ -56,7 +99,14 @@ $("#start").on("click", function() {
   }
 
   if ($("#id_sheets").is(':checked')) {
-    
+    var note = notes[Math.floor(Math.random() * notes.length)];
+    paintGrandStaff(note);
+    runner = window.setInterval(function(){
+      next_notes = notes.slice(0);
+      next_notes.splice(next_notes.indexOf(note), 1);
+      note = next_notes[Math.floor(Math.random() * next_notes.length)]
+      paintGrandStaff(note);
+    }, seconds * 1000);
   } else {
     context = canvas.getContext("2d");
     var note = notes[Math.floor(Math.random() * notes.length)];
@@ -83,4 +133,7 @@ $("#stop").on("click", function() {
   clearInterval(runner);
   context = canvas.getContext("2d");
   context.clearRect(0, 0, canvas.width, canvas.height);
+  if ($("#id_sheets").is(':checked')) {
+    paintGrandStaff(null);
+  }
 })
