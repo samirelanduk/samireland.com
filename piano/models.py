3327923756f753eec6702cab08684a3a2b5fa813
Sam Ireland <sam.ireland.uk@gmail.com>
2016-06-02 17:24:53 +0100

    Add validation to practice session model

--- piano/models.py
+++ piano/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 # Create your models here.
 class PracticeSession(models.Model):
-    minutes = models.IntegerField()
+    minutes = models.IntegerField(validators=[MinValueValidator(1)])
     date = models.DateField(unique=True)
--- piano/tests/test_models.py
+++ piano/tests/test_models.py
@@ -15,3 +15,29 @@ class PracticeSessionTest(TestCase):
 
         retrieved_session = PracticeSession.objects.first()
         self.assertEqual(retrieved_session, session)
+
+
+
+class PracticeSessionValidationTest(TestCase):
+
+    def test_cannot_create_session_without_minutes(self):
+        session = PracticeSession(date=datetime.datetime.now().date())
+        with self.assertRaises(ValidationError):
+            session.full_clean()
+        session = PracticeSession(minutes=0, date=datetime.datetime.now().date())
+        with self.assertRaises(ValidationError):
+            session.full_clean()
+
+
+    def test_cannot_create_post_without_date(self):
+        session = PracticeSession(minutes=5)
+        with self.assertRaises(ValidationError):
+            session.full_clean()
+
+
+    def test_cannot_create_two_posts_with_same_date(self):
+        today = datetime.datetime.now().date()
+        PracticeSession.objects.create(minutes=5, date=today)
+        with self.assertRaises(ValidationError):
+            post = PracticeSession(minutes=10, date=today)
+            post.full_clean()
