bb126639b7412ee0d62e8758f5f2830e1b218579
Sam Ireland <sam.ireland.uk@gmail.com>
2016-06-03 01:50:51 +0100

    Add piano charts

--- piano/views.py
+++ piano/views.py
@@ -15,7 +15,42 @@ def piano_page(request):
     yt_code = yt_html[
      title_start + href_start:title_start + href_start + href_end
     ].split("=")[-1].split('"')[0]
-    return render(request, "piano.html", {"recent_code": yt_code})
+
+    today = datetime.datetime.now().date()
+    sixty_days_ago = today - datetime.timedelta(days=60)
+    sixty_sessions = PracticeSession.objects.all().filter(date__gte=sixty_days_ago).order_by("date")
+    one_year_ago = datetime.datetime(today.year - 1, today.month, today.day).date()
+    year_sessions = PracticeSession.objects.all().filter(date__gte=one_year_ago).order_by("date")
+    cumulative_sixty = [[
+     sixty_days_ago,
+     sum([session.minutes for session in PracticeSession.objects.all().filter(date__lte=sixty_days_ago)])
+    ]]
+    while cumulative_sixty[-1][0] != today:
+        next_day = cumulative_sixty[-1][0] + datetime.timedelta(days=1)
+        cumulative_sixty.append([
+         next_day,
+         sum([session.minutes for session in PracticeSession.objects.all().filter(date__lte=next_day)])
+        ])
+    cumulative_year = [[
+     one_year_ago,
+     sum([session.minutes for session in PracticeSession.objects.all().filter(date__lte=one_year_ago)])
+    ]]
+    while cumulative_year[-1][0] != today:
+        next_day = cumulative_year[-1][0] + datetime.timedelta(days=1)
+        cumulative_year.append([
+         next_day,
+         sum([session.minutes for session in PracticeSession.objects.all().filter(date__lte=next_day)])
+        ])
+    return render(request, "piano.html", {
+     "recent_code": yt_code,
+     "today": today,
+     "sixty_sessions": sixty_sessions,
+     "sixty_days_ago": sixty_days_ago,
+     "year_sessions": year_sessions,
+     "one_year_ago": one_year_ago,
+     "cumulative_sixty": cumulative_sixty,
+     "cumulative_year": cumulative_year
+    })
 
 
 def practice_page(request):
