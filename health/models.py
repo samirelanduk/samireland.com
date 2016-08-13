c232d3e46e600cf93c844cc0e90448affbe3ce57
Sam Ireland <sam.ireland.uk@gmail.com>
2016-08-13 15:57:30 +0100

    Model validation for muscle groups

--- health/models.py
+++ health/models.py
@@ -5,4 +5,4 @@ from django.db import models
 
 # Create your models here.
 class MuscleGroup(models.Model):
-    title = models.TextField()
+    name = models.TextField(unique=True)
--- health/tests/test_models.py
+++ health/tests/test_models.py
@@ -1,48 +1,33 @@
 import datetime
 from django.core.exceptions import ValidationError
 from django.test import TestCase
-from blog.models import BlogPost
+from health.models import MuscleGroup
 
 class ModelCreationTest(TestCase):
 
-    def test_save_and_retrieve_blog_posts(self):
-        self.assertEqual(BlogPost.objects.all().count(), 0)
-        blog_post = BlogPost()
-        blog_post.title = "A post"
-        blog_post.date = datetime.datetime(1939, 9, 1, 5, 0, 0)
-        blog_post.body = "Blah blah blah"
-        blog_post.visible = False
-        blog_post.save()
-        self.assertEqual(BlogPost.objects.all().count(), 1)
+    def test_save_and_retrieve_muscle_groups(self):
+        self.assertEqual(MuscleGroup.objects.all().count(), 0)
+        group = MuscleGroup()
+        group.name = "Arm"
+        group.save()
+        self.assertEqual(MuscleGroup.objects.all().count(), 1)
 
-        retrieved_post = BlogPost.objects.first()
-        self.assertEqual(retrieved_post, blog_post)
+        retrieved_group = MuscleGroup.objects.first()
+        self.assertEqual(retrieved_group, group)
 
 
 
 class ModelValidationTest(TestCase):
 
-    def test_cannot_create_post_without_title(self):
-        blog_post = BlogPost(title="", date=datetime.datetime.now(), body=".", visible=True)
+    def test_cannot_create_group_without_name(self):
+        group = MuscleGroup()
+        group.save()
         with self.assertRaises(ValidationError):
-            blog_post.full_clean()
+            group.full_clean()
 
 
-    def test_cannot_create_post_without_date(self):
-        blog_post = BlogPost(title=".", date="", body=".", visible=True)
+    def test_cannot_create_two_groups_with_same_name(self):
+        MuscleGroup.objects.create(name="arm")
         with self.assertRaises(ValidationError):
-            blog_post.full_clean()
-
-
-    def test_cannot_create_post_without_body(self):
-        blog_post = BlogPost(title="", date=datetime.datetime.now(), body="", visible=True)
-        with self.assertRaises(ValidationError):
-            blog_post.full_clean()
-
-
-    def test_cannot_create_two_posts_with_same_date(self):
-        today = datetime.datetime.now().date()
-        BlogPost.objects.create(title=".", date=today, body=".", visible=True)
-        with self.assertRaises(ValidationError):
-            post = BlogPost(title=".", date=today, body=".", visible=True)
-            post.full_clean()
+            group = MuscleGroup(name="arm")
+            group.full_clean()
