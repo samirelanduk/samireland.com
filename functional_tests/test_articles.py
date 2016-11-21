bdfc3142f6fb006bb29ebcf89986793126a13059
Sam Ireland <sam.ireland.uk@gmail.com>
2016-11-21 23:14:58 +0000

    Add FT for article appearing on front page

--- functional_tests/test_articles.py
+++ functional_tests/test_articles.py
@@ -2,16 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from .base import FunctionalTest
 
-class NewArticleTest(FunctionalTest):
+class ArticleTest(FunctionalTest):
 
-    def test_sam_can_post_article(self):
-        # Sam checks that there are no articles yet
-        self.browser.get(self.live_server_url + "/writing/")
-        article_divs = self.browser.find_elements_by_class_name("article-box")
-        self.assertEqual(len(article_divs), 0)
-
+    def sam_writes_article(self, title, summary, date, body, visible):
         # Sam goes to the new article page
-        self.sam_logs_in()
         self.browser.get(self.live_server_url + "/writing/new/")
 
         # There is a form for entering a article
@@ -28,15 +22,36 @@ class NewArticleTest(FunctionalTest):
         self.assertEqual(live_box.get_attribute("type"), "checkbox")
 
         # Sam write an article
-        title_entry.send_keys("A Title")
-        summary_entry.send_keys("An Intriguing Summary")
-        date_entry.send_keys("24012008")
-        body_entry.send_keys("The body of the beast.")
-        live_box.click()
+        title_entry.send_keys(title)
+        summary_entry.send_keys(summary)
+        date_entry.send_keys(date)
+        body_entry.send_keys(body)
+        if (live_box.is_selected() and not visible) or (not live_box.is_selected() and visible):
+            live_box.click()
 
         # Sam submits and is taken to the writing page
         submit_button.click()
         self.assertEqual(self.current_url, self.live_server_url + "/writing/")
+
+
+
+class NewArticleTest(FunctionalTest):
+
+    def test_sam_can_post_article(self):
+        # Sam checks that there are no articles yet
+        self.browser.get(self.live_server_url + "/writing/")
+        article_divs = self.browser.find_elements_by_class_name("article-box")
+        self.assertEqual(len(article_divs), 0)
+
+        # Sam writes an article
+        self.sam_logs_in()
+        self.sam_writes_article(
+         "A Title",
+         "An Intriguing Summary",
+         "24012008",
+         "The body of the beast.",
+         True
+        )
 
         # There is the article
         article_divs = self.browser.find_elements_by_class_name("article-box")
@@ -55,7 +70,7 @@ class NewArticleTest(FunctionalTest):
         )
 
         # Sam clicks it and is taken to the article
-        article_divs[0].click()
+        article_divs[0].find_element_by_tag_name("article-box-title").click()
         title = self.find_element_by_class_name("article-title")
         summary = self.find_element_by_class_name("article-summary")
         date = self.find_element_by_class_name("article-date")
@@ -64,3 +79,36 @@ class NewArticleTest(FunctionalTest):
         self.assertEqual(summary.text, "An Intriguing Summary")
         self.assertEqual(date.text, "24 January, 2008")
         self.assertEqual(body.text, "The body of the beast.")
+
+
+    def test_new_article_appears_on_home_page(self):
+        # Sam writes an excellent article
+        self.sam_logs_in()
+        self.sam_writes_article("Title", "Summary", "01012001", "Main", True)
+
+        # He goes to the main page and there is the article summary
+        latest_writing = self.browser.find_element_by_id("latest_writing")
+        article_div = latest_writing.find_element_by_class_name("article-box")
+        self.assertEqual(
+         article_div.find_element_by_tag_name("article-box-title").text,
+         "Title"
+        )
+        self.assertEqual(
+         article_div.find_element_by_tag_name("article-box-summary").text,
+         "Summary"
+        )
+        self.assertEqual(
+         article_div.find_element_by_tag_name("article-box-summary").text,
+         "1 January, 2001"
+        )
+
+        # He clicks it and is taken to the article
+        article_div.find_element_by_tag_name("article-box-title").click()
+        title = self.find_element_by_class_name("article-title")
+        summary = self.find_element_by_class_name("article-summary")
+        date = self.find_element_by_class_name("article-date")
+        body = self.find_element_by_class_name("article-body")
+        self.assertEqual(title.text, "Title")
+        self.assertEqual(summary.text, "Summary")
+        self.assertEqual(date.text, "1 January, 2001")
+        self.assertEqual(body.text, "Body")
