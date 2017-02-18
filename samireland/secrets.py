929b49ed9b62caee99afb70d4b1435780d9437f1
Sam Ireland <sam.ireland.uk@gmail.com>
2017-02-18 20:33:04 +0000

    Replace compromised secret key and hide file

--- samireland/secrets.py
+++ samireland/secrets.py
@@ -1,2 +1,2 @@
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(&fxuoasn5=$c=*4wb2#!9+*2i6$9dy^evrc=lg6yk&@l(#wnb'
+SECRET_KEY = "33gd4v4n_$rz+8v6zwvtouq^*e%q0to!%k=-bb)q%uh^zc)_-f"
