676904c478feb24ef43b1dc867c3aa7dfd98cadb
Sam Ireland <sam.ireland.uk@gmail.com>
2017-02-19 13:16:23 +0000

    Add basic navbar

--- ftests/test_home.py
+++ ftests/test_home.py
@@ -15,3 +15,21 @@ class BasePageLayoutTests(FunctionalTest):
         self.browser.get(self.live_server_url + "/")
         header = self.browser.find_element_by_tag_name("header")
         self.assertIn("Sam Ireland", header.text)
+
+
+    def test_nav_is_unordered_list_of_links(self):
+        self.browser.get(self.live_server_url + "/")
+        nav = self.browser.find_element_by_tag_name("nav")
+
+        # The only child of the nav is a <ul>
+        children = nav.find_elements_by_xpath("./*")
+        self.assertEqual(len(children), 1)
+        ul = children[0]
+        self.assertEqual(ul.tag_name, "ul")
+
+        # ul has list of links
+        children = ul.find_elements_by_xpath("./*")
+        self.assertTrue(3 <= len(children) <= 8)
+        for child in children:
+            self.assertEqual(child.tag_name, "li")
+            self.assertEqual(len(child.find_elements_by_tag_name("a")), 1)
