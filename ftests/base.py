02c197892e2d22b693b58c17a59e97fcd089a9d1
Sam Ireland <sam.ireland.uk@gmail.com>
2017-02-24 01:52:30 +0000

    Add FTs for login

--- ftests/base.py
+++ ftests/base.py
@@ -4,9 +4,17 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 class FunctionalTest(StaticLiveServerTestCase):
 
     def setUp(self):
+        self.user = User.objects.create_user(
+         username="testsam",
+         password="testpassword"
+        )
         self.browser = webdriver.Chrome()
         self.browser.set_window_size(350, 600)
 
 
     def tearDown(self):
         self.browser.quit()
+
+
+    def login(self):
+        self.client.login(username="testsam", password="testpassword")
--- ftests/test_home.py
+++ ftests/test_home.py
@@ -223,3 +223,87 @@ class HomePageTests(FunctionalTest):
         self.assertIsNot(children[0].find_element_by_tag_name("img"), None)
         self.assertEqual(children[1].tag_name, "div")
         self.assertEqual(children[1].get_property("id"), "brief-summary")
+
+
+
+class AuthTests(FunctionalTest):
+
+    def test_can_login(self):
+        self.browser.get(self.live_server_url + "/")
+
+        # The 'l' is a link
+        header = self.browser.find_element_by_tag_name("header")
+        link = header.find_element_by_tag_name("a")
+        self.assertEqual(link.text, "l")
+
+        # Clicking it goes to the login page
+        link.click()
+        self.assertEqual(
+         self.browser.current_url,
+         self.live_server_url + "/authenticate/"
+        )
+
+        # There is a login form
+        login_form = self.browser.find_element_by_tag_name("form")
+        name_entry = login_form.find_elements_by_tag_name("input")[0]
+        password_entry = login_form.find_elements_by_tag_name("input")[1]
+        submit_button = login_form.find_elements_by_tag_name("input")[-1]
+
+        # They login
+        name_entry.send_keys("testsam")
+        password_entry.send_keys("testpassword")
+        submit_button.click()
+
+        # They are on the home page
+        self.assertEqual(
+         self.browser.current_url,
+         self.live_server_url + "/"
+        )
+
+        # There is a logout button
+        header = self.browser.find_element_by_tag_name("header")
+        logout_link = header.find_elements_by_tag_name[0]
+        self.assertEqual(logout_link.text, "Logout")
+
+
+    def test_incorrect_login(self):
+        self.browser.get(self.live_server_url + "/authenticate/")
+        login_form = self.browser.find_element_by_tag_name("form")
+        name_entry = login_form.find_elements_by_tag_name("input")[0]
+        password_entry = login_form.find_elements_by_tag_name("input")[1]
+        submit_button = login_form.find_elements_by_tag_name("input")[-1]
+        name_entry.send_keys("badguy1337")
+        password_entry.send_keys("h4ck0r")
+        submit_button.click()
+
+        # The attempt fails. The user weeps, and vows to turn their life around
+        self.assertEqual(
+         self.browser.current_url,
+         self.live_server_url + "/youshallnotpass/"
+        )
+        self.assertIn(
+         "thus far shall you come, and no farther",
+         self.browser.find_element_by_tag_name("main").text.lower()
+        )
+
+
+    def test_can_logout(self):
+        self.login()
+        self.browser.get(self.live_server_url + "/")
+
+        # There is a logout link
+        header = self.browser.find_element_by_tag_name("header")
+        logout_link = header.find_elements_by_tag_name[0]
+
+        # They click it
+        logout_link.click()
+
+        # They are back on the home page
+        self.assertEqual(
+         self.browser.current_url,
+         self.live_server_url + "/"
+        )
+
+        # There are no links in the header
+        header = self.browser.find_element_by_tag_name("header")
+        self.assertEqual(header.find_elements_by_tag_name("a"), [])
