897762c9b2e1c6f61261f85953bdce4c8a573b90
Sam Ireland <sam.ireland.uk@gmail.com>
2017-03-07 20:21:47 +0000

    Home page text uses samdown

--- home/models.py
+++ home/models.py
@@ -1,3 +1,4 @@
+import samdown
 from django.db import models
 
 # Create your models here.
@@ -5,3 +6,7 @@ class EditableText(models.Model):
 
     name = models.CharField(max_length=30)
     content = models.TextField()
+
+    @property
+    def samdown_content(self):
+        return samdown.html_from_markdown(self.content)
--- home/tests/test_models.py
+++ home/tests/test_models.py
@@ -1,3 +1,4 @@
+from unittest.mock import patch
 from samireland.tests import ModelTest
 from home.models import EditableText
 
@@ -12,3 +13,15 @@ class CreationTests(ModelTest):
         self.assertEqual(EditableText.objects.all().count(), 1)
         retrieved_text = EditableText.objects.first()
         self.assertEqual(retrieved_text, text)
+
+
+
+class PropertyTests(ModelTest):
+
+    @patch("samdown.html_from_markdown")
+    def test_editable_text_has_samdown_property(self, mock_converter):
+        mock_converter.return_value = "test output"
+        text = EditableText()
+        text.name = "testsnippet"
+        text.content = "CONTENT"
+        self.assertEqual(text.samdown_content, "test output")
