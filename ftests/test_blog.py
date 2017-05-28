00f99d03d5bd396f1ffe0dbd7b4bbfce47d006e1
Sam Ireland <sam.ireland.uk@gmail.com>
2017-05-28 22:15:24 +0100

    Start 2.3 with basic FTs

---
+++ ftests/test_blog.py
@@ -0,0 +1,74 @@
+"""Contains tests for the blog section."""
+
+from time import sleep
+from datetime import datetime
+from .base import FunctionalTest
+
+class BlogCreationTests(FunctionalTest):
+
+    def test_can_create_blog_post(self):
+        pass
+
+
+    def test_blog_post_needs_correct_date(self):
+        pass
+
+
+    def test_blog_post_needs_correct_title(self):
+        pass
+
+
+    def test_blog_post_needs_correct_body(self):
+        pass
+
+
+
+class BlogReadingTests(FunctionalTest):
+
+    def test_can_cycle_between_blog_posts(self):
+        pass
+
+
+    def test_can_get_blog_posts_by_period(self):
+        pass
+
+
+
+class BlogModificationTests(FunctionalTest):
+
+    def test_can_modify_blog_post(self):
+        pass
+
+
+    def test_modified_blog_post_needs_correct_date(self):
+        pass
+
+
+    def test_modified_blog_post_needs_correct_title(self):
+        pass
+
+
+    def test_modified_blog_post_needs_correct_body(self):
+        pass
+
+
+
+class BlogDeletionTests(FunctionalTest):
+
+    def test_can_delete_blog_post(self):
+        pass
+
+
+
+class BlogAccessTests(FunctionalTest):
+
+    def test_cannot_access_new_blog_page_when_not_logged_in(self):
+        pass
+
+
+    def test_cannot_access_blog_edit_page_when_not_logged_in(self):
+        pass
+
+
+    def test_cannot_access_blog_delete_page_when_not_logged_in(self):
+        pass
