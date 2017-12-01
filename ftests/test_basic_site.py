4aa13d788cb150352d6038bb22588f54cbd5caa6
Sam Ireland <sam.ireland.uk@gmail.com>
2017-12-01 19:47:33 +0000

    Add basic layout

--- ftests/test_basic_site.py
+++ ftests/test_basic_site.py
@@ -10,3 +10,23 @@ class SiteLayoutTests(FunctionalTest):
          [element.tag_name for element in body.find_elements_by_xpath("./*")],
          ["header", "nav", "main", "footer"]
         )
+
+        # The header has the name in it
+        header = self.browser.find_element_by_tag_name("header")
+        self.assertEqual(header.text, "Sam Ireland")
+
+        # The nav has a list of links
+        nav = self.browser.find_element_by_tag_name("nav")
+        ul = nav.find_element_by_tag_name("ul")
+        links = ul.find_elements_by_tag_name("li")
+        self.assertEqual(links[0].text, "Home")
+        self.assertEqual(links[1].text, "Research")
+        self.assertEqual(links[2].text, "Projects")
+        self.assertEqual(links[3].text, "Writing")
+        self.assertEqual(links[4].text, "Blog")
+        self.assertEqual(links[5].text, "About")
+
+        # The footer has a bunch of icons
+        footer = self.browser.find_element_by_tag_name("footer")
+        icons = footer.find_elements_by_class_name("social-icon")
+        self.assertGreaterEqual(len(icons), 4)
