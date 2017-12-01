bab03dbbb840a0221b3075a0c15080c6c58dbeee
Sam Ireland <sam.ireland.uk@gmail.com>
2017-12-01 12:23:21 +0000

    Add local database and apps

--- samireland/settings.py
+++ samireland/settings.py
@@ -1,7 +1,13 @@
 # Settings for samireland.com
 
-from .secrets import SECRET_KEY
+from .secrets import SECRET_KEY, BASE_DIR, DATABASES
 
 ALLOWED_HOSTS = True
 
 ROOT_URLCONF = "samireland.urls"
+
+INSTALLED_APPS = [
+ "django.contrib.contenttypes",
+ "django.contrib.staticfiles",
+ "samireland"
+]
