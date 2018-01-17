d51625a02f353f2816aa0b169e04cb2ded02d2eb
Sam Ireland <sam.ireland.uk@gmail.com>
2018-01-17 13:24:26 +0000

    Style article summaries

--- ftests/test_writing.py
+++ ftests/test_writing.py
@@ -1,4 +1,5 @@
 from .base import FunctionalTest
+from samireland.models import Article
 
 class WritingPageTests(FunctionalTest):
 
@@ -30,6 +31,53 @@ class WritingPageTests(FunctionalTest):
 
     def test_can_change_writing_page_text(self):
         self.check_editable_text("/writing/", "summary")
+
+
+    def test_writing_page_articles(self):
+        Article.objects.create(
+         id="article-1", title="The First Article", date="2016-01-01",
+         summary="summary1", body="Line 1\n\nLine 2"
+        )
+        Article.objects.create(
+         id="article-2", title="The Recent Article", date="2016-08-09",
+         summary="summary2", body="Line 1\n\nLine 2"
+        )
+        Article.objects.create(
+         id="article-3", title="The Middle Article", date="2016-04-12",
+         summary="summary3", body="Line 1\n\nLine 2"
+        )
+
+        # They go to the page and look at the articles - there are 3
+        self.get("/writing/")
+        articles = self.browser.find_element_by_id("articles")
+        articles = articles.find_elements_by_class_name("article")
+        self.assertEqual(len(articles), 3)
+
+        # They are in the correct order
+        self.assertEqual(
+         articles[0].find_element_by_class_name("article-title").text,
+         "The Recent Article"
+        )
+        self.assertEqual(
+         articles[0].find_element_by_class_name("article-date").text,
+         "9 August, 2016"
+        )
+        self.assertEqual(
+         articles[1].find_element_by_class_name("article-title").text,
+         "The Middle Article"
+        )
+        self.assertEqual(
+         articles[1].find_element_by_class_name("article-date").text,
+         "12 April, 2016"
+        )
+        self.assertEqual(
+         articles[2].find_element_by_class_name("article-title").text,
+         "The First Article"
+        )
+        self.assertEqual(
+         articles[2].find_element_by_class_name("article-date").text,
+         "1 January, 2016"
+        )
 
 
 
