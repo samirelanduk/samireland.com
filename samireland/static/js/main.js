9c71f4505bac57e581910105fe66c1aad1c9e0eb
Sam Ireland <sam.ireland.uk@gmail.com>
2018-08-19 09:42:41 +0100

    Make card javascript clickers rather than links due to nested <a> problems

--- samireland/static/js/main.js
+++ samireland/static/js/main.js
@@ -14,25 +14,8 @@ $(window).on("resize", function() {
 });
 
 
-function editText(button, csrf) {
-    var paragraphs = $(button).parent().find("p, figure, ol, ul");
-    paragraphs.each(function(index, p) {
-        p.remove();
-    })
-    button.remove();
-    var form = $(".hidden-form");
-    form.show();
-}
-
-
-function makeDeletionAppear(button) {
-    var form = $(button).parent().find(".hidden-form")[0];
-    console.log(form)
-    $(form).show();
-}
-
-
-function hideDeletion(button) {
-    var form = $(".hidden-form");
-    form.hide();
+function clickCard(url) {
+    if (event.target.nodeName != "A") {
+        window.location.href = url;
+    }
 }
