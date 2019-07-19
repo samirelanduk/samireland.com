ecbed6f6fd3d83f92992c4ad1f2b5bc1cbfe6634
Sam Ireland <sam.ireland.uk@gmail.com>
2019-07-19 13:15:20 +0100

    start about page

--- core/views.py
+++ core/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import redirect, render, get_object_or_404
 from .models import Project, Publication, Article
 
-def home(request, message=False):
+def home(request):
     four_projects = Project.objects.all()[:4]
     two_pubs = Publication.objects.all()[:2]
     article = Article.objects.first()
     return render(request, "home.html", {
      "projects": four_projects, "publications": two_pubs, "article": article
     })
+
+
+def about(request):
+    return render(request, "about.html")
 
 
 def article(request, id):
