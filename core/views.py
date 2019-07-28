558ce7172987cc4a0ca2176a4d2951a69d15361a
Sam Ireland <sam.ireland.uk@gmail.com>
2019-07-28 19:50:58 +0100

    404 page

--- core/views.py
+++ core/views.py
@@ -11,31 +11,35 @@ def home(request):
 
 
 def about(request):
-     periods = Period.objects.all()
-     return render(request, "about.html", {"periods": periods})
+    periods = Period.objects.all()
+    return render(request, "about.html", {"periods": periods})
 
 
 def projects(request):
-     tech = Project.all_tech()
-     projects = Project.objects.all()
-     return render(request, "projects.html", {"tech": tech, "projects": projects})
+    tech = Project.all_tech()
+    projects = Project.objects.all()
+    return render(request, "projects.html", {"tech": tech, "projects": projects})
 
 
 def writing(request):
-     articles = Article.objects.all()
-     return render(request, "writing.html", {"articles": articles})
+    articles = Article.objects.all()
+    return render(request, "writing.html", {"articles": articles})
 
 
 def article(request, id):
-     article = get_object_or_404(Article, id=id)
-     return render(request, "article.html", {"article": article})
+    article = get_object_or_404(Article, id=id)
+    return render(request, "article.html", {"article": article})
 
 
 def research(request):
-     publications = Publication.objects.all()
-     return render(request, "research.html", {"publications": publications})
+    publications = Publication.objects.all()
+    return render(request, "research.html", {"publications": publications})
 
 
 def publication(request, id):
-     publication = get_object_or_404(Publication, id=id)
-     return render(request, "publication.html", {"publication": publication})
+    publication = get_object_or_404(Publication, id=id)
+    return render(request, "publication.html", {"publication": publication})
+
+
+def handler404(request, exception):
+    return render(request, "404.html")
