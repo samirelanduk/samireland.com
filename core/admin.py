38531bf9897da905c177607387c98efe721ac0b9
Sam Ireland <mail@samireland.com>
2023-08-07 22:27:42 +0100

    Reset

--- core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Project)
-admin.site.register(Publication)
-admin.site.register(Article)
-admin.site.register(Period)
-admin.site.register(MediaFile)
