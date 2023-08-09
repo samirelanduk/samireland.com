afd150b455d955d506050878fcd06b9dfb219b76
Sam Ireland <mail@samireland.com>
2023-08-09 22:27:31 +0100

    Wagtail URLs

--- django/core/urls.py
+++ django/core/urls.py
@@ -1,3 +1,12 @@
+from django.conf import settings
+from django.urls import path, include
+from django.conf.urls.static import static
+from wagtail import urls as wagtail_urls
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.documents import urls as wagtaildocs_urls
+
 urlpatterns = [
-    
-]
+    path("admin/", include(wagtailadmin_urls)),
+    path("documents/", include(wagtaildocs_urls)),
+    path("", include(wagtail_urls)),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
