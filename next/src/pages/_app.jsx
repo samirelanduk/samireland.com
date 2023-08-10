96d2a3fe8347e54a7641ad83b91d8cc5d614057e
Sam Ireland <mail@samireland.com>
2023-08-10 18:16:45 +0100

    Basic pages

--- next/src/pages/_app.jsx
+++ next/src/pages/_app.jsx
@@ -1,9 +1,14 @@
 import "@/styles/globals.css"
+import Link from "next/link"
 
 export default function App({ Component, pageProps }) {
   return (
     <div>
-      <nav>samireland.com</nav>
+      <nav>
+        <Link href="/">samireland.com</Link>
+        <Link href="/projects/">Projects</Link>
+        <Link href="/articles/">Articles</Link>
+      </nav>
       <Component {...pageProps} />
     </div>
   )
---
+++ next/src/pages/articles/index.jsx
@@ -0,0 +1,7 @@
+export default function Articles() {
+  return (
+    <main>
+      Articles
+    </main>
+  )
+}
