1c3a9b3adc6987635e88580691e5191a13e44a31
Sam Ireland <mail@samireland.com>
2023-08-11 01:01:29 +0100

    Social links

--- next/src/pages/_app.jsx
+++ next/src/pages/_app.jsx
@@ -10,6 +10,17 @@ export default function App({ Component, pageProps }) {
         <Link href="/articles/">Articles</Link>
       </nav>
       <Component {...pageProps} />
+      <footer className="w-12">
+        <a href="https://twitter.com/samirelanduk">
+          <img src="/images/twitter.svg" alt="Twitter" />
+        </a>
+        <a href="https://instagram.com/samirelanduk">
+          <img src="/images/instagram.svg" alt="Instagram" />
+        </a>
+        <a href="https://www.linkedin.com/in/samirelanduk/">
+          <img src="/images/linkedin.svg" alt="LinkedIn" />
+        </a>
+      </footer>
     </div>
   )
 }
