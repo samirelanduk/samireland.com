491cd5d4cbb0fecb4516a65f8159ff58207c7a2b
Sam Ireland <mail@samireland.com>
2023-09-04 16:35:01 +0100

    404 page

---
+++ next/src/pages/404.jsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function PageNotFound() {
+  return (
+    <main>
+      <Head>
+        <title>Page Not Found - Sam Ireland</title>
+      </Head>
+      <h1>Page Not Found</h1>
+    </main>
+  )
+}
