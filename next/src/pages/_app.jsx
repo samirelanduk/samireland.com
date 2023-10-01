cb3f84e6a42de1ee54aa668662c55243fcab5207
Sam Ireland <mail@samireland.com>
2023-10-01 17:08:26 +0100

    Start style

--- next/src/pages/_app.jsx
+++ next/src/pages/_app.jsx
@@ -3,28 +3,32 @@ import Link from "next/link"
 
 export default function App({ Component, pageProps }) {
   return (
-    <div>
-      <nav>
+    <div className="bg-[#f0f5f0] h-screen flex">
+      <nav className="flex flex-col">
         <Link href="/">samireland.com</Link>
         <Link href="/projects/">Projects</Link>
         <Link href="/about/">About</Link>
         <Link href="/writing/">Writing</Link>
       </nav>
-      <Component {...pageProps} />
-      <footer className="w-12">
-        <a href="https://twitter.com/samirelanduk">
-          <img src="/images/twitter.svg" alt="Twitter" />
-        </a>
-        <a href="https://instagram.com/samirelanduk">
-          <img src="/images/instagram.svg" alt="Instagram" />
-        </a>
-        <a href="https://www.linkedin.com/in/samirelanduk/">
-          <img src="/images/linkedin.svg" alt="LinkedIn" />
-        </a>
-        <a href="/rss" title="Subscribe to RSS feed">
-          RSS
-        </a>
-      </footer>
+      <div className="flex flex-col flex-grow">
+        <div className="flex-grow">
+          <Component {...pageProps} />
+        </div>
+        <footer className="flex h-12 flex-shrink justify-end w-full">
+          <a href="https://twitter.com/samirelanduk">
+            <img src="/images/twitter.svg" alt="Twitter" className="h-full" />
+          </a>
+          <a href="https://instagram.com/samirelanduk">
+            <img src="/images/instagram.svg" alt="Instagram" className="h-full" />
+          </a>
+          <a href="https://www.linkedin.com/in/samirelanduk/">
+            <img src="/images/linkedin.svg" alt="LinkedIn" className="h-full" />
+          </a>
+          <a href="/rss" title="Subscribe to RSS feed">
+            RSS
+          </a>
+        </footer>
+      </div>
     </div>
   )
 }
