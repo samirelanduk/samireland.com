4543c5c0dc82ae21cf3481cef0b58fd35df6d6a3
Sam Ireland <mail@samireland.com>
2023-10-08 21:44:42 +0100

    Style top of writing page

--- next/src/pages/writing/index.jsx
+++ next/src/pages/writing/index.jsx
@@ -4,7 +4,7 @@ import Head from "next/head";
 
 export default function Writing({title, text, articles, meta}) {
   return (
-    <main>
+    <main className="mx-auto max-w-sm sm:max-w-3xl xl:max-w-4xl 3xl:max-w-10xl">
       <Head>
         <title>{`${meta.title} - Sam Ireland`}</title>
         <meta name="description" content={meta.description} />
@@ -17,9 +17,9 @@ export default function Writing({title, text, articles, meta}) {
         <meta property="og:title" content={meta.title} />
         <meta property="og:description" content={meta.description} />
       </Head>
-      <h1>{title}</h1>
-      <div dangerouslySetInnerHTML={{__html: text}} />
-      <div className="flex flex-wrap max-w-sm gap-16 mx-auto sm:max-w-3xl sm:gap-12 md:gap-14 xl:gap-20 xl:max-w-4xl 3xl:max-w-10xl">
+      <h1 className="font-semibold text-3xl font-serif mb-2 sm:text-4xl sm:mb-4">{title}</h1>
+      <div dangerouslySetInnerHTML={{__html: text}}  className="text-base pb-8 mb-10 border-b max-w-4xl sm:text-lg sm:pb-10 sm:mb-12"/>
+      <div className="flex flex-wrap gap-16 sm:gap-12 md:gap-14 xl:gap-20">
         {articles.map(article => <ArticlePreview key={article.title} article={article} />)}
       </div>
     </main>
