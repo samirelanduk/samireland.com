43888b947afde241550d85910ad7c0fa966db6b1
Sam Ireland <mail@samireland.com>
2023-11-24 17:35:47 +0000

    Favicon

--- next/src/pages/_app.jsx
+++ next/src/pages/_app.jsx
@@ -2,6 +2,7 @@ import Footer from "@/components/Footer";
 import Nav from "@/components/Nav"
 import "@/styles/globals.css"
 import { Bitter, Lora, Merriweather, Montserrat, Noto_Serif, Nunito, Open_Sans, Playfair_Display, Poppins, Raleway, Sen } from "next/font/google";
+import Head from "next/head";
 
 const serif = Bitter({
   subsets: ["latin"],
@@ -20,6 +21,9 @@ const sans = Sen({
 export default function App({ Component, pageProps }) {
   return (
     <div className={`flex font-sans ${serif.variable} ${sans.variable}`}>
+      <Head>
+        <link href="/images/favicon.svg" rel="icon" type="image/svg" />
+      </Head>
       <Nav />
       <div className="flex flex-col flex-grow px-2 pt-20 sm:px-6 sm:pt-24 md:px-10 md:pt-28 xl:px-14 xl:pt-32 3xl:pt-36 3xl:px-18">
         <div className="flex-grow">
