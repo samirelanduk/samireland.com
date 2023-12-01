83c543fd6e154fbed6a398fedb07d8576a59eb97
Sam Ireland <mail@samireland.com>
2023-12-01 13:34:40 +0000

    Restructure base layout

--- next/src/pages/_app.jsx
+++ next/src/pages/_app.jsx
@@ -4,6 +4,7 @@ import "@/styles/globals.css"
 import { Bitter, Lora, Merriweather, Montserrat, Noto_Serif, Nunito, Open_Sans, Playfair_Display, Poppins, Raleway, Sen } from "next/font/google";
 import Head from "next/head";
 import { usePathname } from "next/navigation";
+import Div100vh from "react-div-100vh";
 
 const serif = Bitter({
   subsets: ["latin"],
@@ -25,13 +26,13 @@ export default function App({ Component, pageProps }) {
   const isHome = usePathname() === "/";
 
   return (
-    <div className={`flex font-sans ${serif.variable} ${sans.variable}`}>
+    <div className={`font-sans ${serif.variable} ${sans.variable}`}>
       <Head>
         <link href="/images/favicon.svg" rel="icon" type="image/svg" />
       </Head>
-      <Nav />
-      <div className="flex flex-col flex-grow px-4 pt-20 sm:px-6 sm:pt-24 md:px-10 md:pt-28 xl:px-14 xl:pt-32 3xl:pt-36 3xl:px-18">
-        <div className="flex-grow">
+      <div className="flex flex-col">
+        {!isHome && <Nav />}
+        <div className={`flex-grow h-full flex flex-col ${isHome ? "" : "px-4 pt-20 sm:px-6 sm:pt-24 md:px-10 md:pt-28 xl:px-14 xl:pt-32 3xl:pt-36 3xl:px-18 "}`}>
           <Component {...pageProps} />
         </div>
         <Footer isHome={isHome} />
--- next/src/pages/index.jsx
+++ next/src/pages/index.jsx
@@ -1,9 +1,10 @@
 import Head from "next/head";
 import { fetchRemoteData } from "@/fetch";
+import Link from "next/link";
 
 export default function Home({title, about, meta}) {
   return (
-    <main>
+    <main className="flex flex-grow w-full max-w-[100rem] mx-auto flex-col p-4 sm:p-6 md:p-10 xl:p-14">
       <Head>
         <title>{meta.title}</title>
         <meta name="description" content={meta.description} />
@@ -16,9 +17,26 @@ export default function Home({title, about, meta}) {
         <meta property="og:title" content={meta.title} />
         <meta property="og:description" content={meta.description} />
       </Head>
-      <h1>{title}</h1>
-      <div>
-        {about}
+      <div className="lg:flex lg:justify-between">
+        <div>
+          <h1 className="font-serif text-slate-700 text-3xl w-fit border-b-4 border-l-4 border-green-sidc font-semibold mb-6 py-px pl-1 sm:text-4xl sm:mb-8 sm:py-0.5 sm:pl-1.5 md:text-5xl md:mb-10 md:py-1 md:pl-2 md:border-b-8 md:border-l-8 lg:text-6xl lg:mb-12 lg:py-1.5 lg:pl-2.5 xl:text-7xl xl:mb-14 xl:py-2 xl:pl-3">
+            {title}
+          </h1>
+          <div className="text-sm mb-8 sm:text-base md:text-lg lg:text-xl xl:text-2xl">
+            {about}
+          </div>
+        </div>
+        <div className="w-fit flex flex-col text-slate-600 justify-center text-base gap-4 sm:text-lg sm:gap-6 md:text-xl md:gap-8 lg:text-2xl lg:gap-10 xl:test-3xl xl:gap-12">
+          <Link href="/projects/" className="subtle-link">
+            PROJECTS
+          </Link>
+          <Link href="/about/" className="subtle-link">
+            ABOUT ME
+          </Link>
+          <Link href="/writing/" className="subtle-link">
+            WRITING
+          </Link>
+        </div>
       </div>
     </main>
   )
