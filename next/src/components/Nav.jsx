ecc2c6c96a5772486a55bdca017e3dcd8b91167c
Sam Ireland <mail@samireland.com>
2024-01-28 01:52:20 +0000

    Mobile menu

--- next/src/components/Nav.jsx
+++ next/src/components/Nav.jsx
@@ -6,6 +6,7 @@ const Nav = () => {
   const ref = useRef();
 
   const [path, setPath] = useState("");
+  const [menuOpen, setMenuOpen] = useState(true);
 
   useEffect(() => {
     setPath(window.location.pathname);
@@ -31,23 +32,38 @@ const Nav = () => {
       }
     }
     handleScroll();
+    const handleResize = () => setMenuOpen(false);
     window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("resize", handleResize);
+    }
   }, []);
 
   const isWriting = path === "/writing";
   const isProjects = path === "/projects";
   const isAbout = path === "/about";
 
-  const lineClass = "absolute -bottom-0.5 left-0 w-full h-0.5 bg-green-sidc/70";
-  const linkClass = "subtle-link relative";
+  const lineClass = "absolute -bottom-0.5 left-0 w-full h-0.5 bg-green-sidc/70 xs:h-1 xs:-bottom-1";
+  const linkClass = "xs:subtle-link relative";
+
+  const menuClass = "h-1 rounded w-5 transition-width duration-500 ";
 
   return (
-    <nav ref={ref} className="flex fixed z-50 bg-gradient-to-r from-background-2/0 to-background-3/0 top-0 left-0 w-full transition duration-500 justify-between h-12 items-center px-4 sm:px-6 md:px-10 xl:px-14 xl:h-14 3xl:h-16 3xl:px-18">
-      <Link href="/" className="text-sm font-semibold text-slate-800 subtle-link sm:text-base md:text-lg xl:text-xl 3xl:text-2xl">
+    <nav ref={ref} className="flex fixed z-50 bg-gradient-to-r from-background-2/0 to-background-3/0 top-0 left-0 w-full transition duration-500 justify-between h-12 items-center px-8 sm:px-10 md:px-14 xl:px-16 xl:h-14 3xl:h-16 3xl:px-20">
+      <Link href="/" className="text-base font-semibold text-slate-800 subtle-link xs:text-lg md:text-xl xl:text-2xl 3xl:text-3xl">
         Sam Ireland
       </Link>
-      <div className="flex items-center h-full gap-3 text-xs text-slate-600 sm:text-sm sm:gap-3.5 md:text-base md:gap-4 lg:gap-6 xl:gap-8 xl:text-lg 3xl:text-xl 3xl:gap-10">
+      <div className="flex relative z-50 flex-col gap-1 items-end group cursor-pointer xs:hidden" onClick={() => setMenuOpen(!menuOpen)}>
+        <div className={`${menuClass} ${menuOpen ? "bg-white w-7" : "bg-green-sidc w-5 group-hover:w-6" } `} />
+        <div className={`${menuClass} ${menuOpen ? "bg-white w-5" : "bg-green-sidc w-6 group-hover:w-7" } `} />
+        <div className={`${menuClass} ${menuOpen ? "bg-white w-6" : "bg-green-sidc w-7 group-hover:w-5" } `} />
+      </div>
+      <div
+        className={`bg-green-sidc pt-20 gap-y-6 fixed w-full transition-[left] duration-100 ${menuOpen ? "left-0" : "left-full"} right-0 top-0 flex flex-col text-white text-3xl items-center h-full gap-3 xs:static xs:pt-0 xs:w-fit xs:flex-row xs:bg-inherit xs:text-inherit xl:text-slate-600 xs:text-base xs:gap-3.5 sm:gap-6 md:text-lg lg:gap-7 xl:gap-8 xl:text-xl 3xl:text-2xl 3xl:gap-10`}
+        onClick={() => setMenuOpen(false)}
+      >
         <Link href="/projects/" className={linkClass}>
           Projects
           {isProjects && <div className={lineClass} />}
