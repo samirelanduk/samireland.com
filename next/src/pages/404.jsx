6d505ee2e8cd9548d150378cb31a6b63c2d30d22
Sam Ireland <mail@samireland.com>
2024-01-31 19:07:56 +0000

    Style 404 page

--- next/src/pages/404.jsx
+++ next/src/pages/404.jsx
@@ -6,7 +6,10 @@ export default function PageNotFound() {
       <Head>
         <title>Page Not Found - Sam Ireland</title>
       </Head>
-      <h1>Page Not Found</h1>
+      <div className="font-semibold text-center max-w-lg mx-auto text-slate-500 text-xl leading-relaxed py-24 xs:text-2xl xs:leading-relaxed xs:py-20 md:text-3xl md:leading-relaxed md:py-10 xl:text-4xl xl:leading-relaxed xl:py-5 xl:max-w-2xl">
+        Regrettably, this URL does not match any page on this website.
+        You are encouraged to look elsewhere.
+      </div>
     </main>
   )
 }
